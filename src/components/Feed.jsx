73de5050c6cfab38ea5859347c3a43c187d84858
DeepPatel2142 <deeppatel2592@gmail.com>
2023-03-24 15:49:33 -0400

    completed sidebar for clone

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './Sidebar';
 
-const Feed = () => {
+const  Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection: { sx: "column", md: "row"}}}>
+      <Box
+        sx={{height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}}}
+      >
+        <Sidebar/>
+        <Typography className="copyright" varient="body2" sx={{mt: 1.5, color: '#fff'}}>
+          Copyright 2023 Kalupur Darshan
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
-export default Feed
+export default Feed 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,13 +10,13 @@ import CheckroomIcon from '@mui/icons-material/Checkroom';
 import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import TempleHinduIcon from '@mui/icons-material/TempleHindu';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
-
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
+  { name: 'Kalupur Darshan', icon: <TempleHinduIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
